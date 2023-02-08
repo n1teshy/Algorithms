82f793166d285ea39313bcfb34c0fcd849b72aa4
nitesh <nitesh9501@hotmail.com>
2023-02-08 14:25:30 +0530

    Refactor

--- backtracking/sumOfSubsets.js
+++ backtracking/sumOfSubsets.js
@@ -1,23 +1,56 @@
-function _getSubsets({givenSet, requiredSum, currentIndex = 0, currentSum = 0, includedIndices = [], sumOfRest, answers}) {
-    if(currentIndex === givenSet.length || (sumOfRest < (requiredSum - currentSum)) || currentSum > requiredSum){
-        return;
-    }
-    if((currentSum + givenSet[currentIndex]) === requiredSum){
-        answers.push([...includedIndices, currentIndex]);
-    }else{
-        const newSumOfRest = sumOfRest - givenSet[currentIndex];        
-        // currentIndex included
-        _getSubsets({givenSet, requiredSum, currentIndex: (currentIndex + 1), currentSum: (currentSum + givenSet[currentIndex]), includedIndices: [...includedIndices, currentIndex], sumOfRest: newSumOfRest, answers});
-    }
-    // currentIndex not included
-    _getSubsets({givenSet, requiredSum, currentIndex: (currentIndex + 1), currentSum, includedIndices, sumOfRest: sumOfRest - givenSet[currentIndex], answers});
+function _getSubsets({
+  givenSet,
+  requiredSum,
+  currentIndex = 0,
+  currentSum = 0,
+  includedIndices = [],
+  sumOfRest,
+  answers,
+}) {
+  if (
+    currentIndex === givenSet.length ||
+    sumOfRest < requiredSum - currentSum ||
+    currentSum > requiredSum
+  ) {
+    return;
+  }
+  if (currentSum + givenSet[currentIndex] === requiredSum) {
+    answers.push([...includedIndices, currentIndex]);
+  } else {
+    const newSumOfRest = sumOfRest - givenSet[currentIndex];
+    // currentIndex included
+    _getSubsets({
+      givenSet,
+      requiredSum,
+      currentIndex: currentIndex + 1,
+      currentSum: currentSum + givenSet[currentIndex],
+      includedIndices: [...includedIndices, currentIndex],
+      sumOfRest: newSumOfRest,
+      answers,
+    });
+  }
+  // currentIndex not included
+  _getSubsets({
+    givenSet,
+    requiredSum,
+    currentIndex: currentIndex + 1,
+    currentSum,
+    includedIndices,
+    sumOfRest: sumOfRest - givenSet[currentIndex],
+    answers,
+  });
 }
 
 function getSubsets(set, sum) {
-    const answers = [];
-    _getSubsets({givenSet: set, requiredSum: sum, sumOfRest: set.reduce((accumulated, current) => accumulated + current, 0), answers});
-    return answers;
+  const answers = [];
+  _getSubsets({
+    givenSet: set,
+    requiredSum: sum,
+    sumOfRest: set.reduce((accumulated, current) => accumulated + current, 0),
+    answers,
+  });
+  return answers;
 }
-const array = [2,3,1,4,5,7,6,4,5,4,2,4,5,7,7,64,4,6,7,3];
+const array = [2, 3, 1, 4, 5, 7, 6, 4, 5, 4, 2, 4, 5, 7, 7, 64, 4, 6, 7, 3];
 const sum = 13;
 console.log(getSubsets(array, sum));
--- pattern-matching/KMP.js
+++ pattern-matching/KMP.js
@@ -1,35 +1,39 @@
-function matchStr(text, pattern){
-    const LPS = getLPS(pattern);
-    let startMatchIndex = -1;
-    const matches = [];
-    for(let i = 0; i < text.length; i++){
-        if(text[i] === pattern[startMatchIndex + 1]){
-            if(startMatchIndex === (pattern.length - 2)){
-                matches.push([i+1 - (pattern.length), i+1]);
-                startMatchIndex = -1
-            }else{
-                startMatchIndex++;
-            }
-        }else{
-            startMatchIndex = LPS[startMatchIndex + 1];
-        }
+function matchStr(text, pattern) {
+  const LPS = getLPS(pattern);
+  let startMatchIndex = -1;
+  const matches = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === pattern[startMatchIndex + 1]) {
+      if (startMatchIndex === pattern.length - 2) {
+        matches.push([i + 1 - pattern.length, i + 1]);
+        startMatchIndex = -1;
+      } else {
+        startMatchIndex++;
+      }
+    } else {
+      startMatchIndex = LPS[startMatchIndex + 1];
     }
-    return matches;   
+  }
+  return matches;
 }
 
 function getLPS(str) {
-    const arr = [-1];
-    let lastMatchIndex = -1;
-    
-    for(let i = 1; i< str.length; i++){
-        arr.push(-1);
-        if(str[i] === str[lastMatchIndex + 1]){
-            arr[i] = ++lastMatchIndex;
-        }else{
-            lastMatchIndex = -1;
-        }
+  const arr = [-1];
+  let lastMatchIndex = -1;
+
+  for (let i = 1; i < str.length; i++) {
+    arr.push(-1);
+    if (str[i] === str[lastMatchIndex + 1]) {
+      arr[i] = ++lastMatchIndex;
+    } else {
+      lastMatchIndex = -1;
     }
-    return arr;
+  }
+  return arr;
 }
 const text = "abcdefgyoloxyz";
-console.log(matchStr(text, "yolo").map((indices) => text.substring(indices[0], indices[1])))
+console.log(
+  matchStr(text, "yolo").map((indices) =>
+    text.substring(indices[0], indices[1])
+  )
+);
